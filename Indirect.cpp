f0d571683d7c8996022316af2942a6d151871bed
Ondrej Vaic <ondra.vaic@gmail.com>
2020-01-12 22:24:47 +0100

    cd, mkdir with relative and absolute paths

---
+++ Indirect.cpp
@@ -0,0 +1,5 @@
+//
+// Created by me on 1/12/20.
+//
+
+#include "Indirect.h"
---
+++ Indirect.h
@@ -0,0 +1,14 @@
+//
+// Created by me on 1/12/20.
+//
+
+#ifndef ZOS_INDIRECT_H
+#define ZOS_INDIRECT_H
+
+
+class Indirect {
+
+};
+
+
+#endif //ZOS_INDIRECT_H
